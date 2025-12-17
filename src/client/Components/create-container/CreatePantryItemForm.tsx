28e46868a46fbbaceb55ba0988cb545103a007e0
sofiso99 <sofrodasg@gmail.com>
2025-12-17 23:03:11 +0100

    Category dropdown menu still in process

--- src/client/Components/create-container/CreatePantryItemForm.tsx
+++ src/client/Components/create-container/CreatePantryItemForm.tsx
@@ -1,5 +1,4 @@
 import { useForm, type SubmitHandler } from "react-hook-form";
-
 // import { z } from 'zod';
 import "./createContainer.css";
 
@@ -75,13 +74,54 @@ const CreatePantryItemForm = ({ onItemCreated }: CreatePantryItemFormProps) => {
           type="text"
           placeholder="Name"
         />
-        <input
+        <label
           {...register("category", {
             required: true,
           })}
-          type="text"
-          placeholder="Category"
-        />
+        >
+          {/* <input type="text" placeholder="Category" /> */}
+          <select required>
+  
+            <option value="Beverages">Beverages</option>
+            <option value="Dairy & Eggs">Dairy & Eggs</option>
+            <option value="Fruit">Fruit</option>
+            <option value="Frozen Foods">Frozen Foods</option>
+            <option value="Meat and Poultry">Meat and Poultry</option>
+            <option value="Pantry (Canned & Dry Goods)">
+              Pantry (Canned & Dry Goods)
+            </option>
+            <option value="Prepared & Deli Foods">Prepared & Deli Food</option>
+            <option value="Produce">Produce</option>
+            <option value="Seafood">Seafood</option>
+            <option value="Snacks & Sweets">Snacks & Sweets</option>
+            <option value="Vegetables">Vegetables</option>
+            <option value="Other">Other</option>
+          </select>
+        </label>
+
+        {/* To put as dropdown:
+        <label> 
+                <select name="selectedFruit">
+                  <option value="apple">Apple</option>
+                  <option value="banana">Banana</option>
+                  <option value="orange">Orange</option>
+                </select>
+              </label> */}
+
+        {/* Bakery
+              Beverages
+              Dairy & Eggs
+              Fruit
+              Frozen Foods
+              Meat & Poultry
+              Other
+              Pantry (Canned & Dry Goods)
+              Prepared & Deli Foods
+              Produce
+              Seafood
+              Snacks & Sweets
+              Vegetables 
+        */}
 
         <input
           {...register("quantity", {
@@ -97,18 +137,18 @@ const CreatePantryItemForm = ({ onItemCreated }: CreatePantryItemFormProps) => {
         <input
           {...register("notifyWhen", { required: true, valueAsNumber: true })}
           type="number"
-          placeholder="Notify When?"
+          placeholder="Notify When"
         />
         {/* <input
           {...register('expirationDate')}
           type='date'
           placeholder='Expiration Date'
         /> */}
-      <div className="submit-btn-container">
-        <button disabled={isSubmitting} type="submit" className="submit-btn">
-          {isSubmitting ? "Filling your pantry!" : "Submit"}
-        </button>
-      </div>
+        <div className="submit-btn-container">
+          <button disabled={isSubmitting} type="submit" className="submit-btn">
+            {isSubmitting ? "Filling your pantry!" : "Submit"}
+          </button>
+        </div>
       </form>
     </>
   );
